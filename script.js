536723c9f48d1fa2b36640fcc19fb19d4b3c768a
مكتبة الرواد - الصوة <tarek500500@gmail.com>
2025-09-07 09:07:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -685,3 +685,11 @@ style.textContent = `
 `;
 document.head.appendChild(style);
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registered'))
+      .catch(err => console.log('Service Worker registration failed: ', err));
+  });
+}
+
